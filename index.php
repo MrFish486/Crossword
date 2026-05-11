c92358023a9763dd29c8dcbf15eeac9b67afe89c
laszlopav <laszlodeanpav@gmail.com>
2026-05-11 15:35:53 -0700

    Init

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="styles/index.css"></link>
+		<script src="scripts/index.js"></script>
+	</head>
+	<body>
+		<canvas id="viewport" width="500px" height="500px"></canvas>
+		<div class="p"></div>
+		<textarea></textarea>
+	</body>
+</html>
